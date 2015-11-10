119d1240eb9dad31f73f3aa7635db8b005e34f14
zeizyy <yz9fy@virginia.edu>
2015-11-10 18:11:12 -0500

    ss

--- batch.py
+++ batch.py
@@ -7,7 +7,7 @@ from elasticsearch import Elasticsearch
 consumer = KafkaConsumer('event', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 kafka = KafkaClient('kafka:9092')
 producer = SimpleProducer(kafka)
-initial = {'initial':'initial'}
+initial = {'description': 'initial', 'creator_id': 1, 'location': 'initial', 'start_time': '2015-11-09T17:01:16Z', 'event_id': 1, 'name': 'initial'}
 try:
 	producer.send_messages(b'event', json.dumps(initial).encode('utf-8'))
 except:
@@ -15,14 +15,9 @@ except:
 	producer.send_messages(b'event', json.dumps(initial).encode('utf-8'))
 
 es = Elasticsearch(['es'])
-while True:
-	try:
-		for message in consumer:
-			event = json.loads(message.value.decode('utf8'))
-			if 'initial' in event:
-				continue
-			if event:
-				es.index(index='listing_index', doc_type='listing', id=event['event_id'],body=event)
-				print(event," added to es!")
-	except:
-		pass
+
+for message in consumer:
+	event = json.loads(message.value.decode('utf8'))
+	if event:
+		es.index(index='listing_index', doc_type='listing', id=event['event_id'],body=event)
+		print(event," added to es!")
