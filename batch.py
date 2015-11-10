2d552e8caf18c19fb7c9dbbe548b169fdca7f162
zeizyy <yz9fy@virginia.edu>
2015-11-10 13:40:56 -0500

    update

--- batch.py
+++ batch.py
@@ -1,9 +1,16 @@
 import json
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, SimpleProducer, KafkaClient
 
 from elasticsearch import Elasticsearch
 
 consumer = KafkaConsumer('event', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+producer = SimpleProducer(kafka)
+try:
+	producer.send_messages(b'event', json.dumps("").encode('utf-8'))
+except:
+	time.sleep(3)
+	producer.send_messages(b'event', json.dumps("").encode('utf-8'))
+
 es = Elasticsearch(['es'])
 for message in consumer:
 	event = json.loads(message.value.decode('utf8'))
