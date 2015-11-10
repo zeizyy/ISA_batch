95a7be36f8ebfc3e21c0b121cdb910500ece53d8
zeizyy <yz9fy@virginia.edu>
2015-11-10 18:01:34 -0500

    sss

--- batch.py
+++ batch.py
@@ -15,10 +15,14 @@ except:
 	producer.send_messages(b'event', json.dumps(initial).encode('utf-8'))
 
 es = Elasticsearch(['es'])
-for message in consumer:
-	event = json.loads(message.value.decode('utf8'))
-	if 'initial' in event:
-		continue
-	if event:
-		es.index(index='listing_index', doc_type='listing', id=event['event_id'],body=event)
-		print(event," added to es!")
+while True:
+	try:
+		for message in consumer:
+			event = json.loads(message.value.decode('utf8'))
+			if 'initial' in event:
+				continue
+			if event:
+				es.index(index='listing_index', doc_type='listing', id=event['event_id'],body=event)
+				print(event," added to es!")
+	except:
+		pass
