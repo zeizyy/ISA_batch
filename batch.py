120407573df2ce10e1c9463561d43768d3fc8484
zeizyy <yz9fy@virginia.edu>
2015-11-10 18:22:43 -0500

    ddd

--- batch.py
+++ batch.py
@@ -7,15 +7,7 @@ from elasticsearch import Elasticsearch
 consumer = KafkaConsumer('event', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 kafka = KafkaClient('kafka:9092')
 producer = SimpleProducer(kafka)
-initial = {'description': 'initial', 'creator_id': 1, 'location': 'initial', 'start_time': '2015-11-09T17:01:16Z', 'event_id': 1, 'name': 'initial'}
-try:
-	producer.send_messages(b'event', json.dumps(initial).encode('utf-8'))
-except:
-	time.sleep(2)
-	producer.send_messages(b'event', json.dumps(initial).encode('utf-8'))
-
 es = Elasticsearch(['es'])
-time.sleep(10)
 
 for message in consumer:
 	event = json.loads(message.value.decode('utf8'))
